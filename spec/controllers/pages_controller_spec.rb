a60a02867c6ff520f971d69e9ea066c06c50e56b
Rafi Dudekula <Rafi.Dudekula@gmail.com>
2010-11-18 13:13:35 -0600

    Mostly static files

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,17 +1,30 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    
+    it  "should have the right title" do
+      get 'home'
+      rendered.should have_selector("title")
     end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
       response.should be_success
     end
   end
