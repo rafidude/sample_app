e15d1adde11c60f7835816b7c939582896e739f7
Rafi Dudekula <Rafi.Dudekula@gmail.com>
2011-01-07 16:45:00 -0600

    signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 SampleApp::Application.routes.draw do
+  root :to => 'pages#home'
   match "help", :to => 'pages#help'
   match "contact", :to => 'pages#contact'
   match "about", :to => 'pages#help'
-  root :to => 'pages#home'
+  match "signup", :to => 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
